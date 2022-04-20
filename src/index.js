2910903092d43108e9e16bf13a6fea4875f669ad
liuxiang7 <liuxiang7@tal.com>
2022-04-20 08:47:10 +0800

    组件练习-学生列表

--- src/index.js
+++ src/index.js
@@ -1,51 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import src1 from "./assets/1.jpg";
-import src2 from "./assets/2.jpg";
-import src3 from "./assets/3.jpg";
-import "./index.css";
-
-const srcs = [src1, src2, src3]; //保存图片路径的数组
-
-let index = 0; //显示的图片索引
-
-const container = document.getElementById('root');
-
-let timer; //计时器
-
+import StudentList from "./components/StudentList";
+const appkey = "demo13_1545210570249";
 /**
- * 根据index的值，显示某张图片
+ * 获取所有的学生数据
  */
-function render() {
-    ReactDOM.render(<img src={srcs[index]} alt="" />, container);
+async function fetchAllStudents() {
+    const stus = await fetch("http://api.duyiedu.com/api/student/findAll?appkey=" + appkey)
+        .then(resp => resp.json()).then(resp => resp.data)
+    return stus;
 }
 
-/**
- * 启动计时器，每隔一段时间，切换图片
- */
-function start() {
-    stop();
-    timer = setInterval(() => {
-        index = (index + 1) % 3; //改变index
-        render();
-    }, 2000);
-}
-
-/**
- * 停止计时器
- */
-function stop(){
-    clearInterval(timer);
+async function render() {
+    ReactDOM.render("正在加载中....", document.getElementById('root'));
+    const stus = await fetchAllStudents();//获取学生数组
+    ReactDOM.render(<StudentList stus={stus} />, document.getElementById('root'));
 }
 
 render();
 
-start();
 
-container.onmouseenter = function(){
-    stop();
-}
 
-container.onmouseleave = function(){
-    start();
-}
