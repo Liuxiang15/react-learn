0a169692f1e8a0c501d05ad36e373925e28b34bb
liuxiang7 <liuxiang7@tal.com>
2022-06-27 23:01:39 +0800

    9-2.react-redux查询学生

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { Provider } from "react-redux"
+import store from "./store"
+import StudentSearch from "./components/StudentSearch"
+
 export default function App() {
     return (
-        <div></div>
+        <Provider store={store}>
+            <StudentSearch />
+        </Provider>
     )
 }
--- src/store/saga/counterTask.js
+++ src/store/saga/counterTask.js
@@ -1,18 +1,17 @@
 import { takeEvery, delay, put } from "redux-saga/effects"
-import { asyncIncrease, asyncDecrease, increase, decrease } from "../action/counter"
+import { actionTypes, increase, decrease } from "../action/counter"
 
-function* asyncIncreaseSaga() {
+function* asyncIncrease() {
     yield delay(2000);
     yield put(increase())
 }
 
-function* asyncDecreaseSaga() {
+function* asyncDecrease() {
     yield delay(2000);
     yield put(decrease())
 }
 
 export default function* () {
-    yield takeEvery(asyncIncrease.toString(), asyncIncreaseSaga)
-    yield takeEvery(asyncDecrease.toString(), asyncDecreaseSaga)
-    console.log("正在监听asyncIncrease、asyncDecrease")
+    yield takeEvery(actionTypes.asyncIncrease, asyncIncrease)
+    yield takeEvery(actionTypes.asyncDecrease, asyncDecrease)
 }
